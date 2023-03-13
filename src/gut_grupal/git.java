63efd04c70dfedc14d14f4191c4d2a6a501e9acd
AdrianHernandez <adrian.s323317@cesurformacion.com>
2023-03-13 15:58:49 +0100

    segundo commit

--- src/gut_grupal/git.java
+++ src/gut_grupal/git.java
@@ -3,6 +3,7 @@ package gut_grupal;
 public class git {
 	public static void main(String[] args) {
 		System.out.println("repositorio creado");
+		System.out.println("primer commit realizado");
 	}
 }
 
